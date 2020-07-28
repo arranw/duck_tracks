268edbcf0be9f51720f6452970c3960804c7dc74
Arran Woodruff <arranwoodruff@gmail.com>
2020-07-28 11:12:06 -0600

    small changes to tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -30,15 +30,20 @@ const mockData = [
 
 describe("Feeding Endpoints", () => {
   beforeEach(async () => {
+    // before each test
+
+    // clear the database
     await models.feeding.destroy({
       where: {},
       truncate: true
     });
 
+    // insert a record for each of our mock data points
     mockData.forEach(async data => await models.feeding.create(data));
   });
 
   it("should create a new feeding", async () => {
+    // add the first mock data point
     const res = await request(app).post("/api/feedings").send(mockData[0]);
 
     expect(res.statusCode).toEqual(201);
@@ -46,22 +51,24 @@ describe("Feeding Endpoints", () => {
   });
 
   it("should return a specific feeding", async () => {
+    // retrieve the first record in the database
     const res = await request(app).get("/api/feedings/1").send();
 
     expect(res.statusCode).toEqual(200);
 
     expect(res.body.id).toBe(1);
-    expect(res.body.time).toBe("14:13");
-    expect(res.body.location).toBe("Rotary Park");
-    expect(res.body.duck_quantity).toBe(5);
-    expect(res.body.food_type).toBe("Bread");
-    expect(res.body.food_quantity).toBe(350);
+    expect(res.body.time).toBe(mockData[0].time);
+    expect(res.body.location).toBe(mockData[0].location);
+    expect(res.body.duck_quantity).toBe(mockData[0].duck_quantity);
+    expect(res.body.food_type).toBe(mockData[0].food_type);
+    expect(res.body.food_quantity).toBe(mockData[0].food_quantity);
   });
 
   it("should return array of feedings", async () => {
     const res = await request(app).get("/api/feedings").send();
 
     expect(res.statusCode).toEqual(200);
+    // ensure the model comes back with the correct attributes
     expect(res.body).toContainEqual(
       expect.objectContaining({
         id: expect.anything(),
